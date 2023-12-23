8cdac32c14015f8b958a0a05de8288fa8fb258ab
FarzeenKist <farzeenkistnareddy@gmail.com>
2023-12-24 02:41:47 +0400

    Some improvements

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,79 @@
-import { Canister, Record, nat64, Principal, query, text, update, Void, Vec } from 'azle';
+import { Canister, Record, init, ic, nat64, Principal, query, text, update, Void, Vec, Result, Err, Ok } from 'azle';
 
-type Log = {
-    account: Principal
-    description: text
-    amount: nat64
+const Log = Record({
+    account: Principal,
+    description: text,
+    amount: nat64,
+});
+
+type Log = typeof Log;
+
+let admin: Principal;
+let balance: nat64 = 0n;
+let logs: Vec<Log> = [];
+
+
+function isCallerAdmin(): boolean{
+    return admin.toText() === ic.caller().toText()
 }
 
-let balance: nat64 = 0n;
-let logs: Log[] = [];
-
 export default Canister({
-    pay: update([Principal, nat64], Void, (addrs, amount) => {
+    init: init([], () => {
+        admin = ic.caller();
+    }),
+    // Function to add a payment log to the canister
+    pay: update([Principal, nat64], Result(Log, text), (addrs, amount) => {
+        if (!isCallerAdmin()){
+            return Err("Not admin")
+        }
+        if (amount == BigInt(0)){
+            return Err("Amount has to be greater than zero")
+        }
+        // payment logs can only be created only if the balance is not less than the amount
         if(balance >= amount){
             balance -= amount;
-            logs.push({
+            let log = {
                 account: addrs,
                 description: "out",
                 amount: amount
-            })
+            }
+            logs.push(log)
+            return Ok(log)
         }else{
-            throw new Error("Not enough balance")
+            return Err("Not enough balance")
         }
     }),
-
-    getPaid: update([Principal, nat64], Void, (addrs, amount) => {
+    // Function to add an income log to the canister
+    getPaid: update([Principal, nat64], Result(Log, text), (addrs, amount) => {
+        if (!isCallerAdmin()){
+            return Err("Not admin")
+        }
+        if (amount == BigInt(0)){
+            return Err("Amount has to be greater than zero")
+        }
         balance += amount;
-        logs.push({
+        let log = {
             account: addrs,
             description: "in",
             amount: amount
-        })
+        }
+        logs.push(log)
+        return Ok(log)
     }),
 
-    getLogs: query([], Vec(Record({
-        account: Principal,
-        description: text,
-        amount: nat64
-    })), () => {
-        return logs;
+    // Function to query all logs stored in the canister
+    getLogs: query([], Result(Vec(Log), text), () => {
+        if (!isCallerAdmin()){
+            return Err("Not admin")
+        }
+        return Ok(logs);
     }),
-
-    getBalance: query([], nat64, () => {
-        return balance;
+    // Function to get the balance
+    getBalance: query([], Result(nat64, text), () => {
+        if (!isCallerAdmin()){
+            return Err("Not admin")
+        }
+        return Ok(balance);
     })
 });
 
